7b256a86c91fca2ca0e5e94d5e40b4084a6a8ef8
fenixmiranda <fenixacmiranda@gmail.com>
2022-09-05 23:13:52 -0300

    dicas

---
+++ 15 - JavaScript (básico)/aula4/script.js
@@ -0,0 +1,11 @@
+
+//comentario de uma única linha com o '//'
+
+/*
+
+comentário com várias linhas
+
+*/
+
+
+console.log('Hello World!') //Não é mais necessário o ponto e vírgula no JavaScript, virou opcional. Se for utilizar, manter o padrão.
