5cd9ad67e414cba752805fa9f1acfc7ada19d66d
fenixmiranda <fenixacmiranda@gmail.com>
2022-09-05 23:35:55 -0300

    váriaveis

---
+++ 15 - JavaScript (básico)/aula5/script.js
@@ -0,0 +1,27 @@
+
+
+//var, let e const
+
+//string
+//var nome = 'Fênix'
+
+//number
+//var idade = 35 
+
+// let nome = 'Fênix Miranda'
+
+// nome = 'Fulano' //Posso subscrever uma variavel sem o let
+
+const nome = 'Fênix' // o const nunca posso alterar depois exemplo embaixo
+
+//Utilizar quando for mais de uma palavra o padrão Camel case, exemplo:
+
+let nomeCompleto = 'Fenix Artur de Carvalho Miranda'
+
+//Variaveis não pode iniciar com número, mas podemos iniciar com o "underlaine --> _ , exemplo:"
+
+const _nome = 'Fenix'
+
+// nome = 'fulano' //Não consigo subscrever uma váriavel declarada com const
+
+console.log(nome)
