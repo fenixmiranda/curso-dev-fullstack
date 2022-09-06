f52c0044f96dfc3502aa59ffafa025008be74775
fenixmiranda <fenixacmiranda@gmail.com>
2022-09-05 23:19:45 -0300

    statements

--- 15 - JavaScript (básico)/aula4/script.js
+++ 15 - JavaScript (básico)/aula4/script.js
@@ -3,9 +3,39 @@
 
 /*
 
-comentário com várias linhas
+comentário com multiplas linhas
+
+*/
+
+/* 
+Um programa de computador é uma lista de instruções a ser executada por um computador
+
+Um Programa JavaScript é uma lista de instruções de programação.
+
+valores, operadores, expressões, palavras-chave e comentários.
+
+break
+continue
+debugger
+do ... while
+for
+function
+if ...
+return
+switch
+try ...
+var
+let
+const
+
+
 
 */
 
 
 console.log('Hello World!') //Não é mais necessário o ponto e vírgula no JavaScript, virou opcional. Se for utilizar, manter o padrão.
+console.log('Hello World2')
+
+
+document.querySelector('#elemento').src = 'imagem.jpg'
+
