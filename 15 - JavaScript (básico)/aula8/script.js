f596dd9908a8e065a20b32a8ca2c01c043843a5f
fenixmiranda <fenixacmiranda@gmail.com>
2022-09-07 19:25:56 -0300

    operadores comparativos

---
+++ 15 - JavaScript (básico)/aula8/script.js
@@ -0,0 +1,25 @@
+/*
+
+== igualdade valores
+
+=== igualdade de valores e tipos
+
+!= diferença de valores
+
+!== diferença de valores e tipos
+
+> maior que
+
+
+< menor que
+
+>= maior ou igual
+
+<= menor ou igual
+
+*/
+
+let a = 10
+let b = 5
+
+console.log(a <= b)
