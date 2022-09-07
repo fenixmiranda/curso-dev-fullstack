e20e083df291045e2e5a765dab8ce67ae26abd46
fenixmiranda <fenixacmiranda@gmail.com>
2022-09-07 19:42:31 -0300

    condicionais if | else if | else

---
+++ 15 - JavaScript (básico)/aula9/script.js
@@ -0,0 +1,7 @@
+let nome = prompt ('Digite seu nome')
+
+if (nome === 'Fenix' || nome === 'fenix') {
+    document.write(`Olá ${nome}, seja bem vindo! `)
+} else {
+    document.write('Você não tem acesso de administrador!')
+}
