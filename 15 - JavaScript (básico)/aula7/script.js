85e80973060042403a1c2dd3cd0b1ff5f245724f
fenixmiranda <fenixacmiranda@gmail.com>
2022-09-07 19:18:26 -0300

    operadores aritiméticos

---
+++ 15 - JavaScript (básico)/aula7/script.js
@@ -0,0 +1,74 @@
+/*
+
+adição: +
+subtração: -
+multiplicação: *
+divisão: /
+incremento: ++
+decremento: --
+
+atribuição: +=  -=
+
+
+*/
+
+//adição
+
+let a = 10
+let b = 5
+let c = a + b
+
+console.log(c)
+
+//NaN = Not a Number
+
+//subtração
+
+let d = 10
+let e = 5
+let f = d - e
+
+console.log(f)
+
+// multiplicação
+let g = 10
+let h = 5
+let i = g * h
+
+console.log(i)
+
+// divisão
+
+let j = 19
+let k = 3
+let l = j / k
+
+console.log(l)
+
+//Incremento
+
+let m = 10
+m++
+console.log(m)
+
+
+//decremento
+
+let n = 10
+n--
+console.log(n)
+
+//atribuição +
+
+let o = 12
+o += 25 // o = o + 25
+
+console.log(o)
+
+//atribuição -
+
+let p = 12
+p -= 25 // o = o - 25
+
+console.log(p)
+
