8b1827a488b62a7aed30060e2fd3955171b9fd65
fenixmiranda <fenixacmiranda@gmail.com>
2022-09-08 18:13:24 -0300

    exercicio3

---
+++ 15 - JavaScript (básico)/exercicio3/script.js
@@ -0,0 +1,27 @@
+
+
+
+function chamarPrompt() {
+    let nome = prompt('Digite seu nome:')
+    let idade = prompt('Digite sua idade:')
+
+    let maiorIdade = 18
+
+    if (idade >= maiorIdade) {
+       
+            document.querySelector("#texto").innerHTML=`Olá ${nome}, você é maior de idade.`
+    } 
+    else {
+
+            document.querySelector("#texto").innerHTML=`Olá ${nome}, você é menor de idade.`
+        
+    }
+
+    if (nome.toLowerCase()==='thomas anderson') {
+        document.querySelector("#texto2").innerHTML=`Você é personagem do filme The Matrix!`
+    }
+
+}
+
+
+
