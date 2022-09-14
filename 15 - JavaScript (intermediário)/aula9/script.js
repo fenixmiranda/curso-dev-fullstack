33ffaff37c3234f74197d9a177a237119a2b43e7
fenixmiranda <fenixacmiranda@gmail.com>
2022-09-13 23:54:31 -0300

    Form parte 1

---
+++ 15 - JavaScript (intermediário)/aula9/script.js
@@ -0,0 +1,21 @@
+
+
+let formulario = document.querySelector('#formCadastro')
+
+formulario.onsubmit = function(evento) {
+    evento.preventDefault ()
+
+    let inputNome = document.forms['formCadastro'] ['nome']
+
+    if (!inputNome.value) {
+        inputNome.classList.add('inputError')
+
+        let span = inputNome.nextSibling.nextSibling
+        span.innerText = 'Digite o nome corretamente'
+    } else {
+        inputNome.classList.remove('inputError')
+        
+        let span = inputNome.nextSibling.nextSibling
+        span.innerText = ''
+    }
+}
