48fb7c2a5895a9d4a45f1406ea1d9e11525b1e1a
fenixmiranda <fenixacmiranda@gmail.com>
2022-10-25 19:43:27 -0300

    FETCH

---
+++ 18 - JavaScript (Avançado)/aula13/script.js
@@ -0,0 +1,27 @@
+/* 
+
+FETCH
+
+*/
+
+function transformarEmJson(response) {
+    return response.json()
+}
+
+function exibirNaTela(dados) {
+    console.log(dados)
+}
+
+function exibirErro() {
+    console.log('Ops, deu erro!')
+}
+
+const botaoCarregar = document.querySelector('#botaoCarregar')
+
+botaoCarregar.onclick = () => 
+    fetch('https://jsonplaceholder.typicode.com/photos', { 
+        method: 'GET',
+    })
+        .then(transformarEmJson)
+        .then(exibirNaTela)
+        .catch(exibirErro)
